520c4bd70dc0ac90c4d7f69b0c71ae1a3631e9a5
pedrooliveira23 <pedrooliveira2309@gmail.com>
2015-11-20 20:16:09 -0200

    Separando os arquivos

---
+++ AlcoolGasolina/controller/Controlador.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/AlcoolGasolina")
+public class Controlador extends HttpServlet {
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String paramAlcool = req.getParameter("alcool");
+		Double alcool = paramAlcool == null ? 0.0 : Double
+				.parseDouble(paramAlcool);
+
+		String paramGasolina = req.getParameter("gasolina");
+		Double gasolina = paramGasolina == null ? 0.0 : Double
+				.parseDouble(paramGasolina);
+
+		CalculadoraRelacao calc = new CalculadoraRelacao(alcool, gasolina);
+		calc.calcularRelacao();
+		calc.calcularResultado();
+
+		req.setAttribute("calcular", calc); // Passando um objeto para o JSP.
+
+		// Chamar o JSP apenas para mostrar o resultado.
+		req.getRequestDispatcher("AlcoolGasolina/index.jsp").forward(req, resp);
+	}
+
+}
